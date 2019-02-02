08b9affbb60224c383d55c1c30bf69d15f8668b3
Rajesh Hadiya <hadiarajesh007@gmail.com>
2019-02-02 17:40:08 +0530

    Update fibonacciSequence.kt

--- fibonacciSequence.kt
+++ fibonacciSequence.kt
@@ -3,6 +3,7 @@ fun main(arr : Array<String>) {
     var fibonacciSequence = generateSequence(1 to 1) {
 	//Calculating Fn  = Fn-1 + Fn-2 and mapping to return Pair<Int,Int>
         it.second to it.first + it.second
-    }.map{ it.second } //Getting first element of next sequence
+    }.map{ it.second } 
+
     println("First X Fibonacci numbers are : 0,1, ${fibonacciSequence.take(20).joinToString()}")
 }
