ffb3994b41ef3e40e3c5f3931cc446929b76b013
Rajesh Hadiya <hadiarajesh007@gmail.com>
2019-02-03 17:24:11 +0530

    Update fibonacciSequence.kt

--- fibonacciSequence.kt
+++ fibonacciSequence.kt
@@ -1,6 +1,6 @@
 fun main(arr : Array<String>) {
 	//Generating Sequence starting from 1
-    var fibonacciSequence = generateSequence(1 to 1) {
+    val fibonacciSequence = generateSequence(1 to 1) {
 	//Calculating Fn  = Fn-1 + Fn-2 and mapping to return Pair<Int,Int>
         it.second to it.first + it.second
     }.map{ it.second } 
